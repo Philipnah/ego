1189795b0f38e1d843029474c3c1b745321d7d52
Philip Han <46556419+Philipnah@users.noreply.github.com>
2024-08-01 16:19:10 +0200

    Add bar chart & json structure for prices

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"time"
 
 	structures "github.com/Philipnah/ego/structures"
 
@@ -14,29 +15,45 @@ import (
 func main() {
 	// Emission stuff
 	emissionUrl := "https://api.energidataservice.dk/dataset/CO2Emis"
-	emissionApiArguments := "?end=now%2BP1D"
+
+	currentTime := time.Now()
+	date := currentTime.Format("2006-01-02")
+
+	emissionApiArguments := "?start=" + date + "&end=now%2BP1D&sort=Minutes5DK%20ASC"
 	emissionEndpoint := emissionUrl + emissionApiArguments
 	// end of emission stuff
 	// Price stuff
-	priceUrl := "https://api.energidataservice.dk/dataset/DatahubPricelist"
-	priceApiArguments := "?end=now%2BP1D&sort=ValidFrom%20DESC"
-	priceEndpoint := priceUrl + priceApiArguments
-	fmt.Print(priceEndpoint)
+	// priceUrl := "https://api.energidataservice.dk/dataset/DatahubPricelist"
+	// priceApiArguments := "?end=now%2BP1D&sort=ValidFrom%20DESC"
+	// priceEndpoint := priceUrl + priceApiArguments
 	// end of price stuff
 
-	area, err := pterm.DefaultArea.WithCenter().Start("Getting electricity price...")
-	if err != nil {
-		fmt.Print("pterm area could not be created")
-		panic(err)
-	}
+	pterm.DefaultSection.Println("CO2 Emissions")
 
+	loadEmissions(emissionEndpoint)
+}
+
+func loadEmissions(endpoint string) {
+	spinner, _ := pterm.DefaultSpinner.Start("Current CO2 emissions: ...")
+	spinner.ShowTimer = true
+
+	// Get the json data from the API
 	emissionsData := structures.Emissions{}
-	error := getJson(emissionEndpoint, &emissionsData)
+	error := getJson(endpoint, &emissionsData)
 	if error != nil {
 		panic(error)
 	}
 
-	area.Update(pterm.Sprintf("Electricity price: %v", emissionsData.Records[1].CO2Emission))
+	// Get just the emissions number right now
+	currentEmissions, _ := currentEmissions(&emissionsData)
+
+	spinner.UpdateText(pterm.Sprintf("Current CO2 emissions: %v g/kWh", currentEmissions))
+	spinner.Success()
+
+	bars := getBars(&emissionsData)
+
+	pterm.DefaultBarChart.WithHorizontal().WithWidth(15).WithBars(bars).WithShowValue().Render()
+
 }
 
 func getJson(endpoint string, target interface{}) (err error) {
@@ -53,4 +70,43 @@ func getJson(endpoint string, target interface{}) (err error) {
 	responseBody, err := io.ReadAll(response.Body)
 
 	return json.Unmarshal(responseBody, target)
+}
+
+func currentEmissions(data *structures.Emissions) (result float64, err error) {
+	// Get the last record in the data to get the current emissions
+	return data.Records[len(data.Records)-1].CO2Emission, nil
+}
+
+func previousEmissions(data *structures.Emissions) (emis []float64, time []string, err error) {
+	emis = make([]float64, len(data.Records))
+	time = make([]string, len(data.Records))
+
+	// TODO: Get only DK1 data
+	// TODO: Combine emissions to hourly averages
+
+	for i, record := range data.Records {
+		emis[i] = record.CO2Emission
+		time[i] = record.Minutes5DK
+	}
+
+	return emis, time, nil
+}
+
+func getBars(data *structures.Emissions) []pterm.Bar {
+	prevEmis, prevTime, _ := previousEmissions(data)
+
+	bars := []pterm.Bar{}
+	for i := 0; i < len(prevEmis); i++ {
+		bars = append(bars, pterm.Bar{Label: prevTime[i], Value: int(prevEmis[i])})
+	}
+
+	return bars
+}
+
+func avgEmissions(data *structures.Emissions) (result float64, err error) {
+	panic("Not implemented yet")
+}
+
+func lowHighEmissions(data *structures.Emissions) (low float64, high float64, err error) {
+	panic("Not implemented yet")
 }
--- structures/structures.go
+++ structures/structures.go
@@ -11,3 +11,48 @@ type Emissions struct {
 		CO2Emission float64
 	}
 }
+
+type Prices struct {
+	Total   int
+	Sort    string
+	Limit   int
+	Dataset string
+	Records []struct {
+		ChargeOwner          string  `json:"ChargeOwner"`
+		GLNNumber            string  `json:"GLN_Number"`
+		ChargeType           string  `json:"ChargeType"`
+		ChargeTypeCode       string  `json:"ChargeTypeCode"`
+		Note                 string  `json:"Note"`
+		Description          string  `json:"Description"`
+		ValidFrom            string  `json:"ValidFrom"`
+		ValidTo              string  `json:"ValidTo"`
+		VATClass             string  `json:"VATClass"`
+		Price1               float64 `json:"Price1"`
+		Price2               float64 `json:"Price2"`
+		Price3               float64 `json:"Price3"`
+		Price4               float64 `json:"Price4"`
+		Price5               float64 `json:"Price5"`
+		Price6               float64 `json:"Price6"`
+		Price7               float64 `json:"Price7"`
+		Price8               float64 `json:"Price8"`
+		Price9               float64 `json:"Price9"`
+		Price10              float64 `json:"Price10"`
+		Price11              float64 `json:"Price11"`
+		Price12              float64 `json:"Price12"`
+		Price13              float64 `json:"Price13"`
+		Price14              float64 `json:"Price14"`
+		Price15              float64 `json:"Price15"`
+		Price16              float64 `json:"Price16"`
+		Price17              float64 `json:"Price17"`
+		Price18              float64 `json:"Price18"`
+		Price19              float64 `json:"Price19"`
+		Price20              float64 `json:"Price20"`
+		Price21              float64 `json:"Price21"`
+		Price22              float64 `json:"Price22"`
+		Price23              float64 `json:"Price23"`
+		Price24              float64 `json:"Price24"`
+		TransparentInvoicing int     `json:"TransparentInvoicing"`
+		TaxIndicator         int     `json:"TaxIndicator"`
+		ResolutionDuration   string  `json:"ResolutionDuration"`
+	}
+}
